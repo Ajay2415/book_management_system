2c94f89a43fbbf5b1a75315a375d89464f982cd8
x22175270 <x22175270@student.ncirl.ie>
2024-07-17 00:46:19 +0530

    static files

--- book_management_system/urls.py
+++ book_management_system/urls.py
@@ -22,5 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("bookapp.urls")),
-]
-#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
